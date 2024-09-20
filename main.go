67038723a08a66b2e08e72739402de0cfa2cc92d
purylte <purylte@gmail.com>
2024-09-20 12:32:41 +0000

    upload image

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -10,7 +14,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", rootHandler)
-
+	mux.HandleFunc("/upload", uploadHandler)
 	log.Fatal(http.ListenAndServe(":3000", mux))
 
 }
@@ -18,4 +22,33 @@ func main() {
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	component := templates.MainLayout()
 	component.Render(r.Context(), w)
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "expected post", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseMultipartForm(10 << 20) // 10MB max file size
+	if err != nil {
+		http.Error(w, "parse form failed", http.StatusBadRequest)
+		return
+	}
+
+	file, header, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "fail to retrieve file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	buf := bytes.NewBuffer(nil)
+	if _, err := io.Copy(buf, file); err != nil {
+		http.Error(w, "fail to copy to buffer", http.StatusInternalServerError)
+	}
+	imgBase64 := base64.StdEncoding.EncodeToString(buf.Bytes())
+	contentType := header.Header.Get("Content-Type")
+	dataURL := fmt.Sprintf("data:%s;base64,%s", contentType, imgBase64)
+	templates.Image(dataURL).Render(r.Context(), w)
 }
