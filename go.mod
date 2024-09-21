563a2c778a5f3aafe8289554e1d6dfcd68e864d0
purylte <purylte@gmail.com>
2024-09-21 13:39:01 +0000

    add file server

--- go.mod
+++ go.mod
@@ -5,3 +5,8 @@ go 1.22.3
 require gocv.io/x/gocv v0.37.0
 
 require github.com/a-h/templ v0.2.778
+
+require (
+	github.com/alexedwards/scs/v2 v2.8.0 // indirect
+	github.com/otiai10/gosseract/v2 v2.4.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
-	"bytes"
-	"encoding/base64"
-	"fmt"
-	"io"
+	"crypto/rand"
+	"encoding/hex"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/purylte/ocr-webui/templates"
 )
 
+var sessionManager *scs.SessionManager
+
+const tempImageDir = "tmp/img"
+
 func main() {
+	sessionManager = scs.New()
+	sessionManager.Lifetime = 24 * time.Hour
+
+	if err := os.MkdirAll(tempImageDir, os.ModePerm); err != nil {
+		log.Fatalf("cannot create temp image dir: %v", err)
+	}
+
 	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(tempImageDir))))
 	mux.HandleFunc("/", rootHandler)
 	mux.HandleFunc("/upload", uploadHandler)
 	log.Fatal(http.ListenAndServe(":3000", mux))
@@ -32,23 +46,46 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	err := r.ParseMultipartForm(10 << 20) // 10MB max file size
 	if err != nil {
-		http.Error(w, "parse form failed", http.StatusBadRequest)
+		http.Error(w, "parse form failed "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	file, header, err := r.FormFile("image")
+	file, handler, err := r.FormFile("image")
 	if err != nil {
-		http.Error(w, "fail to retrieve file", http.StatusBadRequest)
+		http.Error(w, "fail to retrieve file "+err.Error(), http.StatusBadRequest)
 		return
 	}
 	defer file.Close()
 
-	buf := bytes.NewBuffer(nil)
-	if _, err := io.Copy(buf, file); err != nil {
-		http.Error(w, "fail to copy to buffer", http.StatusInternalServerError)
+	// buf := bytes.NewBuffer(nil)
+	// if _, err := io.Copy(buf, file); err != nil {
+	// 	http.Error(w, "fail to copy to buffer", http.StatusInternalServerError)
+	// }
+	// imgBase64 := base64.StdEncoding.EncodeToString(buf.Bytes())
+	// contentType := handler.Header.Get("Content-Type")
+	// dataURL := fmt.Sprintf("data:%s;base64,%s", contentType, imgBase64)
+	// templates.Image(dataURL).Render(r.Context(), w)
+	randBytes := make([]byte, 4)
+	rand.Read(randBytes)
+	randFileName := hex.EncodeToString(randBytes) + filepath.Ext(handler.Filename)
+	filePath := filepath.Join(tempImageDir, randFileName)
+	dst, err := os.Create(filePath)
+	if err != nil {
+		http.Error(w, "unable to save file "+err.Error(), http.StatusInternalServerError)
+		return
 	}
-	imgBase64 := base64.StdEncoding.EncodeToString(buf.Bytes())
-	contentType := header.Header.Get("Content-Type")
-	dataURL := fmt.Sprintf("data:%s;base64,%s", contentType, imgBase64)
-	templates.Image(dataURL).Render(r.Context(), w)
+	defer dst.Close()
+
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		http.Error(w, "unable to process file "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, err = dst.ReadFrom(file)
+	if err != nil {
+		http.Error(w, "unable to save file "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	templates.Image("static/"+randFileName).Render(r.Context(), w)
+
 }
